47f2e83469c04ff41e3a6df46a8555aed60ebd8d
Debashish Mohanty <112998176+Debashish2001@users.noreply.github.com>
2023-11-14 05:22:21 +0530

    Add files via upload

---
+++ tk.py
@@ -0,0 +1,10 @@
+from tkinter import *
+deba_root= Tk()
+deba_root.geometry("733x434")
+deba_root.minsize(733,434)
+deba_root.maxsize(733,434)
+deba= Label(text="Wellcome to Deba")
+deba.pack()
+
+
+deba_root.mainloop()
